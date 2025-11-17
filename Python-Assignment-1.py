34010507b695a4b7c06300f400471106e4af4a99
mohanasudhashanmugam <mohanasudha.s@gmail.com>
2025-11-17 14:21:54 +0530

    Add files via upload

---
+++ Python-Assignment-1.py
@@ -0,0 +1,137 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "2e1a6762",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "##Print statement"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "15a6985e",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Welcome to Assignment-1\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"Welcome to Assignment-1\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "cc6f30dd",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "##Addition of two numbers"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 10,
+   "id": "9a935979",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Num1= 10\n",
+      "Num2= 30\n",
+      "Add= 40\n"
+     ]
+    }
+   ],
+   "source": [
+    "Num1,Num2=10,30\n",
+    "print(\"Num1= \"+ str(Num1)+\"\\n\" +\"Num2= \"+str(Num2) +\"\\n\"+ \"Add= \"+str(Num1+Num2))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "89426342",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "##Body Mass Index"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 14,
+   "id": "424882f8",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Enter the BMI index:34\n",
+      "Very Overweight\n"
+     ]
+    }
+   ],
+   "source": [
+    "BMI=int(input(\"Enter the BMI index:\"))\n",
+    "if(BMI<18.5):\n",
+    "    print(\"Underweight\")\n",
+    "elif(BMI>=18.5 and BMI<=24.9):\n",
+    "    print(\"Healthyweight\")\n",
+    "elif(BMI>=25 and BMI<=29.9):\n",
+    "    print(\"Overweight\")\n",
+    "else:\n",
+    "    print(\"Very Overweight\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "4b1e260a",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "4f45719d",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.8"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
